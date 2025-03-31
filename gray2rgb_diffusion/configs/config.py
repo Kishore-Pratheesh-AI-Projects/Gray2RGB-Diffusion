cd8c4103934a6cfabcfc06d1cd432b4d01e99a93
Kishore Sampath <skishore2602.dev@gmail.com>
2025-03-31 00:25:58 -0400

    Added config files

---
+++ gray2rgb_diffusion/configs/config.py
@@ -0,0 +1,17 @@
+import torch
+
+config = {
+    "image_size": 32,
+    "do_subset_train": False,
+    "train_subset_size": 10,
+    "batch_size": 64,
+    "learning_rate": 3e-4,
+    "num_epochs": 10,
+    "num_train_timesteps": 1000,
+    "beta_schedule": "linear",
+    "block_out_channels": (64, 128, 256, 512),
+    "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+    "save_dir": "./saved_models",
+    "do_subset_test": True,
+    "test_subset_size": 512
+}
